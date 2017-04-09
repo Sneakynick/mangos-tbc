118fa642c5b232520e332e9f08cca333a831fa18
Sneakynick <crhansen12@apu.edu>
2017-04-09 16:23:57 -0700

    Scripting for fight in Mechanar
    Nethermancer Sepethrea's Fire Dudes were not casting their Inferno abilities. Added C++ case in SpellAuras to handle this.
    Also 2 db changes to fix the other spell and implement threat dropping (they agro randomly)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -1377,6 +1377,14 @@ void Aura::TriggerSpell()
                             triggerTarget->CastSpell(triggerTarget, spell_id, TRIGGERED_OLD_TRIGGERED, nullptr, this, casterGUID);
                         return;
                     }
+//                    // Inferno (normal and heroic)
+                    case 35268:
+                    case 39346:
+                    {
+                        int32 damage = auraSpellInfo->EffectBasePoints[0];
+                        triggerTarget->CastCustomSpell(triggerTarget, 35283, &damage, nullptr, nullptr, TRIGGERED_OLD_TRIGGERED, nullptr, this, casterGUID);
+                        return;
+                    }
 //                    // Gravity Lapse
 //                    case 34480: break;
 //                    // Tornado
@@ -1389,8 +1397,6 @@ void Aura::TriggerSpell()
 //                    case 35016: break;
 //                    // Interrupt Shutdown
 //                    case 35176: break;
-//                    // Inferno
-//                    case 35268: break;
 //                    // Salaadin's Tesla
 //                    case 35515: break;
 //                    // Ethereal Channel (Red)
@@ -1514,8 +1520,6 @@ void Aura::TriggerSpell()
 //                    case 39259: break;
 //                    // Hellfire - The Exorcism, Jules releases darkness, aura
 //                    case 39306: break;
-//                    // Inferno
-//                    case 39346: break;
 //                    // Enchanted Weapons
 //                    case 39489: break;
 //                    // Shadow Bolt Whirl
